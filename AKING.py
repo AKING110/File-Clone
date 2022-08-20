de64d994507e145a8536f76f1cfb70db7f124958
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-08-20 15:23:51 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -2,6 +2,7 @@ import os,platform
 os.system('clear')
 print('[•] Checking Updates...')
 os.system('git pull')
+os.system('clear')
 print(' [×] This Commands Off\n [+] New Commands')
 print(' cd $HOME')
 print(' git clone https://github.com/AKING110/AKING')
