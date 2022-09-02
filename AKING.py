7dc52490c57099e1aaeb08c70a8b52825ec0a641
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-09-02 17:17:14 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -2,8 +2,10 @@ import os,platform
 os.system('clear')
 print('[•] Checking Updates...')
 os.system('git pull')
-os.system('clear')
-print(' [×] This Commands Off\n [+] New Commands')
-os.system('cd $HOME')
-os.system('git clone https://github.com/AKING110/AKING')
-os.system('cd AKING && python AKING.py')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
+    import AKING
+else:
+    exit('\033[1;31m[×] Sorry Device Not Support')
